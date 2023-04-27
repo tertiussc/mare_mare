c3d39919ec083bcae6dbbd67493d3c9276183825
Tertius Scheepers <tertiussc@gmail.com>
2023-04-27 10:51:01 +0200

    fixed social media menu

--- wp-content/themes/mare-mare/functions.php
+++ wp-content/themes/mare-mare/functions.php
@@ -20,4 +20,17 @@ function products_per_page($products)
 }
 add_filter('loop_shop_per_page', 'products_per_page', 20);
 
-remove_action('storefront_footer', 'storefront_credit');
+// Remove the default wooCommerce footer and add new one
+function mare_footer (){
+    remove_action('storefront_footer', 'storefront_credit', 20);
+    add_action('storefront_after_footer', 'mare_new_footer', 10);
+}
+add_action('init', 'mare_footer',10);
+
+
+// Add new footer
+function mare_new_footer (){
+    echo "<div class='mare-footer'>";
+    echo "<p>All rights reserved &copy; Mare-Mare" . " " . get_the_date('Y') . "</p>";
+    echo "</div>";
+}
